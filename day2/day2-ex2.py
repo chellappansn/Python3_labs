4a3aeb75b78466741cd91edd176d9addbdc7ae43
Chellappan Sampath <chellappan.sn@gmail.com>
2024-11-19 19:31:15 +0000

    day2 ex2

--- day2/day2-ex2.py
+++ day2/day2-ex2.py
@@ -9,13 +9,21 @@
 
 master_pin = "0123"
 pin = None
+attempts = 0
 
-while pin != master_pin:
+while pin != master_pin and attempts < 3:
     pin = input("Enter PIN:")
 
     if pin == master_pin:
         print("Valid PIN")
+        break # Breaks the entire loop
     else:
         print("Invalid PIN ", pin)
+
+    attempts += 1
+    print("Attempts: ", attempts)
+else:
+    # Execute only once when condition becomes False
+    print("Too many attempts")
 
 print("Done.")
