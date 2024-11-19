a11450bf5c7e8a36e008d64a00f12675adf92618
Chellappan Sampath <chellappan.sn@gmail.com>
2024-11-19 00:57:45 +0000

    day2 ex2

---
+++ day1-ex3.py
@@ -0,0 +1,12 @@
+#! /usr/bin/env python3
+# Author: Chellappan Sampath
+# Version: 1.0
+# Description: Ex 2
+
+fname = input("Enter first name:")
+lname = input("Enter last name :")
+
+print(fname,lname)
+
+names=[fname,lname]
+print(names)
---
+++ day1-ex4.py
@@ -0,0 +1,10 @@
+#! /usr/bin/env python3
+# Author: Chellappan Sampath
+# Version: 1.0
+# Description: Day1 Ex2
+
+var = input("Enter a value:")
+
+print(var.upper())
+print(len(var))
+print(var.isdecimal())
